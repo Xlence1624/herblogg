d7351a49955e1ab05f1eaa94e97baa5a90541c5e
Orji Michael <86269930+Xlence1624@users.noreply.github.com>
2026-09-08 22:27:50 +0100

    Add readTime field to post model

--- models/post.js
+++ models/post.js
@@ -24,6 +24,10 @@ const postSchema = new mongoose.Schema(
       type: String,
       default: null,
     },
+      readTime: {
+      type: String,
+      default: null,
+    },
 
        imagePublicId: {
       type: String,
